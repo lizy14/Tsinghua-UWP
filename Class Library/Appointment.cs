2bbdade25e98fa9b5f01f75290b8f45140b4780a
Zhaoyang Li <lizy14@yeah.net>
2017-02-21 16:46:50 +0800

    bug fix: mergeAppointments

--- Class Library/Appointment.cs
+++ Class Library/Appointment.cs
@@ -228,7 +228,8 @@ namespace TsinghuaUWP {
                     if (i + offset >= n) break;
                     var current = input[i + offset];
                     if (current.Subject == starting.Subject && current.Location == starting.Location) {
-                        if (starting.StartTime + starting.Duration - current.StartTime < TimeSpan.FromMinutes(21)) {
+                        var gap = current.StartTime - starting.StartTime - starting.Duration;
+                        if (gap < TimeSpan.FromMinutes(21) && gap > TimeSpan.FromMinutes(-1)) {
                             starting.Duration = current.StartTime + current.Duration - starting.StartTime;
                             offset++;
                         } else {
